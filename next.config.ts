a2e17f468b5edf2999ce54966f5084cd06baa643
tonipcv <xppsalvador@gmail.com>
2024-10-27 13:23:43 -0300

    sem eslint

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  /* outras opções de configuração aqui */
 };
 
 export default nextConfig;
