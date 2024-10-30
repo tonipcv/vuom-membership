3a28ab0aa37daf40bdca9df0a905f1b58896efa6
tonipcv <116813368+tonipcv@users.noreply.github.com>
2024-10-30 10:41:51 -0300

    Delete src/app/dash directory

--- src/app/dash/metadata.ts
+++
@@ -1,9 +0,0 @@
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Dashboard',
-  description: 'Gerencie sua conta e visualize suas informações',
-  robots: {
-    index: false,
-  },
-};
